cc69e8bc69fb55883e9ec63c8437cc6392616cc0
albimdkr <albimudakar04@gmail.com>
2024-01-30 19:03:30 +0700

    feat: inisialization JavaScript

---
+++ scripts/script.js
@@ -0,0 +1,139 @@
+/**
+ * [
+ *    {
+ *      id: <int>
+ *      task: <string>
+ *      timestamp: <string>
+ *      isCompleted: <boolean>
+ *    }
+ * ]
+ */
+const todos = [];
+const RENDER_EVENT = 'render-todo';
+const SAVED_EVENT = 'saved-todo';
+const DELETED_EVENT = 'deleted-todo'
+const UNDO_EVENT = 'undo-todo';
+const STORAGE_KEY = 'TODO_APPS';
+
+function generateId() {
+  return +new Date();
+}
+
+function generateTodoObject(id, task, description, timestamp, isCompleted) {
+  return {
+    id,
+    task,
+    description,
+    timestamp,
+    isCompleted,
+  };
+}
+
+function findTodo(todoId) {
+  for (const todoItem of todos) {
+    if (todoItem.id === todoId) {
+      return todoItem;
+    }
+  }
+  return null;
+}
+
+function findTodoIndex(todoId) {
+  for (const index in todos) {
+    if (todos[index].id === todoId) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+/**
+ * Fungsi ini digunakan untuk memeriksa apakah localStorage didukung oleh browser atau tidak
+ *
+ * @returns boolean
+ */
+function isStorageExist() /* boolean */ {
+  if (typeof Storage === undefined) {
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Fungsi ini digunakan untuk menyimpan data ke localStorage
+ * berdasarkan KEY yang sudah ditetapkan sebelumnya.
+ */
+function saveData() {
+  if (isStorageExist()) {
+    const parsed /* string */ = JSON.stringify(todos);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+  }
+}
+
+/**
+ * Fungsi ini digunakan untuk memuat data dari localStorage
+ * Dan memasukkan data hasil parsing ke variabel {@see todos}
+ */
+function loadDataFromStorage() {
+  const serializedData /* string */ = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const todo of data) {
+      todos.push(todo);
+    }
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function makeTodo(todoObject) {
+  const { id, task, description, timestamp, isCompleted } = todoObject;
+
+  const textTitle = document.createElement('h2');
+  textTitle.innerText = task;
+
+  const textDescription = document.createElement('p');
+  textDescription.innerText = description;
+
+  const textTimestamp = document.createElement('p');
+  textTimestamp.innerText = timestamp;
+
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('inner');
+  textContainer.append(textTitle, textDescription, textTimestamp);
+
+  const container = document.createElement('div');
+  container.classList.add('item', 'shadow');
+  container.append(textContainer);
+  container.setAttribute('id', `todo-${id}`);
+
+  if (isCompleted) {
+    const undoButton = document.createElement('button');
+    undoButton.classList.add('undo-button');
+    undoButton.addEventListener('click', function () {
+      undoTaskFromCompleted(id);
+    });
+
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('trash-button');
+    trashButton.addEventListener('click', function () {
+      removeTaskFromCompleted(id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+    const checkButton = document.createElement('button');
+    checkButton.classList.add('check-button');
+    checkButton.addEventListener('click', function () {
+      addTaskToCompleted(id);
+    });
+
+    container.append(checkButton);
+  }
+
+  return container;
+}
+
