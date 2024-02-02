70397690bb2465821b8c1bf60792e4c663b5abb3
albimdkr <albimudakar04@gmail.com>
2024-02-02 20:03:30 +0700

    feat: adding eventlistener costume RENDER_EVENT

--- scripts/script.js
+++ scripts/script.js
@@ -253,3 +253,22 @@ function handleValidationSuccess(messageSuccesed) {
     icon: 'success',
   });
 }
+
+// Costume Event RENDER_EVENT
+document.addEventListener(RENDER_EVENT, function () {
+  const uncompletedTODOList = document.getElementById('todos');
+  const listCompleted = document.getElementById('completed-todos');
+
+  // clearing list item
+  uncompletedTODOList.innerHTML = '';
+  listCompleted.innerHTML = '';
+
+  for (const todoItem of todos) {
+    const todoElement = makeTodo(todoItem);
+    if (todoItem.isCompleted) {
+      listCompleted.append(todoElement);
+    } else {
+      uncompletedTODOList.append(todoElement);
+    }
+  }
+});
